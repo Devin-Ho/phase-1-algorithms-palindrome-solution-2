e3d3ffdc1e706bdb51214797ccce661c398a4c5d
Devin Ho <djxho36@gmail.com>
2022-05-01 12:52:26 -0400

    commit tree 182018b60f02aa33ef6096f683b70803425e9036

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  for (let i = 0; i< word.length / 2; i++){
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+
+    if (startChar !== endChar) return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  If the first letter is the same as the last letter, and the second letter is the same as the second to last letter, etc.
+  Until we reach the middle, return true 
+
+  Iterate from the beginning to the middle 
+    check each letter to the corresponding letter from the end
+      if any letters don't match, return false 
+  
+  return true
 */
 
 /*
@@ -18,8 +35,29 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
+
 
 module.exports = isPalindrome;
